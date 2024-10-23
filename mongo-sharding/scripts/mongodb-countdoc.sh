8a32dc528ecc68677a1dd4212e9b7973a2f62ef5
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-23 21:20:34 +0300

    Update mongodb-countdoc.sh

--- mongo-sharding/scripts/mongodb-countdoc.sh
+++ mongo-sharding/scripts/mongodb-countdoc.sh
@@ -5,7 +5,7 @@ use somedb
 db.helloDoc.countDocuments()
 EOF
 
-docker compose exec -T shard2 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 EOF
