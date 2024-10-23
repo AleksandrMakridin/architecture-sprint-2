dc2fbdb5a33db594a4a5ef8e845742779d80d266
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-23 21:28:43 +0300

    Update mongodb-countdoc.sh

--- mongo-sharding-repl/scripts/mongodb-countdoc.sh
+++ mongo-sharding-repl/scripts/mongodb-countdoc.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-###
-# Тест бд
-###
 
-docker compose exec -T shard1_1 mongosh --port 27010 --quiet <<EOF
+docker compose exec -T shard1_1 mongosh --port 27018 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
 
-docker compose exec -T shard1_2 mongosh --port 27011 --quiet <<EOF
+docker compose exec -T shard1_2 mongosh --port 27019 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
 
-docker compose exec -T shard1_3 mongosh --port 27012 --quiet <<EOF
+docker compose exec -T shard1_3 mongosh --port 27022 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
 
 
-docker compose exec -T shard2_1 mongosh --port 27013 --quiet <<EOF
+docker compose exec -T shard2_1 mongosh --port 27023 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
 
-docker compose exec -T shard2_2 mongosh --port 27014 --quiet <<EOF
+docker compose exec -T shard2_2 mongosh --port 27024 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
 
-docker compose exec -T shard2_3 mongosh --port 27015 --quiet <<EOF
+docker compose exec -T shard2_3 mongosh --port 27025 --quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 EOF
