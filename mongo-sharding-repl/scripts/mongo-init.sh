e190ac3142685f2239ecb8917fe51f65c1e57e52
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-23 21:27:00 +0300

    Update mongo-init.sh

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -18,14 +18,14 @@ EOF
 
 sleep 1
 
-docker compose exec -T shard1_1 mongosh --port 27010 --quiet <<EOF
+docker compose exec -T shard1_1 mongosh --port 27018 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1_1:27010" },
-        { _id : 1, host : "shard1_2:27011" },
-        { _id : 2, host : "shard1_3:27012" },
+        { _id : 0, host : "shard1_1:27018" },
+        { _id : 1, host : "shard1_2:27019" },
+        { _id : 2, host : "shard1_3:27022" },
       ]
     }
 );
@@ -33,14 +33,14 @@ EOF
 
 sleep 1
 
-docker compose exec -T shard2_1 mongosh --port 27013 --quiet <<EOF
+docker compose exec -T shard2_1 mongosh --port 27023 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 0, host : "shard2_1:27013" },
-        { _id : 1, host : "shard2_2:27014" },
-        { _id : 2, host : "shard2_3:27015" }
+        { _id : 0, host : "shard2_1:27023" },
+        { _id : 1, host : "shard2_2:27024" },
+        { _id : 2, host : "shard2_3:27025" }
       ]
     }
 );
@@ -52,17 +52,16 @@ EOF
 ###
 
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
-sh.addShard("shard1/shard1_1:27010");
-sh.addShard("shard1/shard1_2:27011");
-sh.addShard("shard1/shard1_3:27012");
-sh.addShard("shard2/shard2_1:27013");
-sh.addShard("shard2/shard2_2:27014");
-sh.addShard("shard2/shard2_3:27015");
+sh.addShard("shard1/shard1_1:27018");
+sh.addShard("shard1/shard1_2:27019");
+sh.addShard("shard1/shard1_3:27022");
+sh.addShard("shard2/shard2_1:27023");
+sh.addShard("shard2/shard2_2:27024");
+sh.addShard("shard2/shard2_3:27025");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb;
 
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 
-db.helloDoc.countDocuments();
 EOF
