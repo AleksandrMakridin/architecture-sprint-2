f214cd3712148ff7a79e707334ebd1a133bf3a58
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-23 21:19:58 +0300

    Update mongo-init.sh

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -41,13 +41,7 @@ sh.addShard( "shard2/shard2:27019");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
-EOF
 
-###
-# Инициализируем бд
-###
-
-docker compose exec -T mongodb1 mongosh <<EOF
 use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
