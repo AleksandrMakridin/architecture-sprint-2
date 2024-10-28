db18f44167d3c015137007fa0adb4ee93744e0a6
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-28 21:13:43 +0300

    Update mongo-init.sh
    Оставлена только инициализация

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
-docker exec -T configSrv mongosh --port 27017 <<EOF
+#configSrv
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
 rs.initiate(
   {
     _id : "config_server",
-       configsvr: true,
+    configsvr: true,
     members: [
       { _id : 0, host : "configSrv:27017" }
     ]
@@ -13,6 +13,7 @@ rs.initiate(
 );
 EOF
 
+#shard1
 docker exec -T shard1 mongosh --port 27018 <<EOF
 rs.initiate(
   {
@@ -23,7 +24,7 @@ rs.initiate(
   }
 );
 EOF
-
+#shard2
 docker exec -T shard2 mongosh --port 27019 <<EOF
 rs.initiate(
   {
@@ -34,15 +35,7 @@ rs.initiate(
   }
 );
 EOF
-
-docker exec -T mongos_router mongosh --port 27020 <<EOF
+#router
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
 sh.addShard( "shard1/shard1:27018");
 sh.addShard( "shard2/shard2:27019");
-
-sh.enableSharding("somedb");
-sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
-
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
-EOF
-
