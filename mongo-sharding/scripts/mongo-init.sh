d7f7735965a3db93aea02a4978cd6c28507c0451
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-28 22:20:52 +0300

    Update mongo-init.sh
    Поправил разбмвку на shard-ы (1.1 - 2.1)

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -14,23 +14,27 @@ rs.initiate(
 EOF
 
 #shard1
-docker compose exec -T shard1 mongosh --port 27018 <<EOF
+docker compose exec -T shard1_1 mongosh --port 27018 <<EOF
 rs.initiate(
   {
     _id : "shard1",
     members: [
-      { _id : 0, host : "shard1:27018" },
+      { _id : 0, host : "shard1_1:27018" },
+      { _id : 0, host : "shard1_2:27018" },
+      { _id : 0, host : "shard1_3:27018" },      
     ]
   }
 );
 EOF
 #shard2
-docker compose exec -T shard2 mongosh --port 27019 <<EOF
+docker compose exec -T shard2_1 mongosh --port 27023 <<EOF
 rs.initiate(
   {
     _id : "shard2",
     members: [
-      { _id : 1, host : "shard2:27019" }
+      { _id : 1, host : "shard2_1:27023" },
+      { _id : 1, host : "shard2_2:27023" },
+      { _id : 1, host : "shard2_3:27023" },      
     ]
   }
 );
@@ -38,7 +42,7 @@ EOF
 #router
 docker compose exec -T mongos_router mongosh --port 27020 <<EOF
 sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "shard2/shard2:27023");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
