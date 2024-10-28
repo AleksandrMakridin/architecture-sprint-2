c18186285206c1eca494c5ee68c4db532c13bf85
AleksandrMakridin <a.v.makridin@gmail.com>
2024-10-28 22:54:44 +0300

    Update mongo-init.sh
    shard 1 - 2

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -42,7 +42,6 @@ rs.initiate(
 );
 EOF
 
-
 sleep 1
 
 docker compose exec -T redis redis-cli
@@ -50,11 +49,12 @@ EOF
 
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
 sh.addShard("shard1/shard1_1:27018");
-sh.addShard("shard1/shard1_2:27019");
+sh.addShard("shard2/shard1_1:27019");
 sh.addShard("shard1/shard1_3:27022");
 sh.addShard("shard2/shard2_1:27023");
 sh.addShard("shard2/shard2_2:27024");
 sh.addShard("shard2/shard2_3:27025");
+
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb;
